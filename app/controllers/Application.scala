1d5e41f7861ddeef13b39d87fb93c0da82030a7b
Lachezar Yankov <lucho870601@gmail.com>
2014-07-31 23:06:49 +0200

    Initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import models._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    //Ok(views.html.index("Your new application is ready."))
+    //Ok("Hello World!")
+    Redirect(routes.Application.tasks)
+  }
+
+  def tasks = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+  }
+
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+
+  }
+
+  def deleteTask(id: Long) = Action.apply({
+    Task.delete(id)
+    Redirect(routes.Application.tasks)
+  })
+
+  val taskForm = Form("label" -> nonEmptyText)
+}
